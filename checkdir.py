b94115a4ec62b4d6f1697a2fbc89c3b90c62bca9
Immunity-Project <gkm24@drexel.edu>
2016-05-13 17:38:52 -0400

    added a little sandbox to test out stuff
    try experimental version with that script

--- checkdir.py
+++ checkdir.py
@@ -66,6 +66,22 @@ def CheckDirChanges(): #check directory is empty, else print files/folders liste
             SetFirstRun()
 
 
+def GetUSB():
+    device_re = re.compile("Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
+    if os.name == 'nt':
+        #df = subprocess.check_output("lsusb", shell=True) windows equivelent
+    else:
+        df = subprocess.check_output("lsusb", shell=True)
+    devices = []
+    for i in df.split('\n'):
+        if i:
+            info = device_re.match(i)
+            if info:
+                dinfo = info.groupdict()
+                dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
+                devices.append(dinfo)
+    print devices
+
 
 ####### MAIN SCRIPT #######
 
@@ -206,9 +222,10 @@ class Application(Frame):
         self.grid()
         self.createWidgets()
         self.LoopDir()
+
         
-        
-        
+
+GetUSB()
 
 root = Tk()
 entry = Entry()
