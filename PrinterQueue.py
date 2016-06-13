5061bc8072c4c38c865c71175049d50a8f7476a2
Immunity-Project <gkm24@drexel.edu>
2016-06-13 11:13:27 -0400

    Organized and cleanup part 1
    pushing to laptop, almost done with optimization and fixing for python 3

--- PrinterQueue.py
+++ PrinterQueue.py
@@ -2,18 +2,4 @@
 #This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, #You can obtain one at https://mozilla.org/MPL/2.0/
 
 def CurrentFileName():
-	currentFileName=[]
-	return currentFileName
-def CurrentFilePath():
-	currentFilePath=[]
-	return currentFilePath
-def CurrentPrintNumber():
-	currentPrintNumber=[]
-	return currentPrintNumber
-def CurrentPrintType():
-	currentPrintType=[]
-	return currentPrintType
-CurrentFileName()
-CurrentFilePath()
-CurrentPrintNumber()
-CurrentPrintType()
+	currentFileName=
